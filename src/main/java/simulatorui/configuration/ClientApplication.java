14a9c85b454d6ac99a7249a831463aac72b129e4
Juan Camilo Rada <jcamilorada@live.com>
2014-10-17 15:03:27 -0500

    Created client project application

---
+++ src/main/java/simulatorui/configuration/ClientApplication.java
@@ -0,0 +1,17 @@
+package simulatorui.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+
+public class ClientApplication
+{
+    public static void main(String... args)
+    {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
