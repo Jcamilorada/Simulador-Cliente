298d4dd1a8d910343b3205714b9d551b8e45cfc2
Juan Rada <Jcamilorada@live.com>
2016-06-14 20:57:58 -0500

    changed packaging mode to war mode

--- src/main/java/simulatorui/configuration/ClientApplication.java
+++ src/main/java/simulatorui/configuration/ClientApplication.java
@@ -2,14 +2,22 @@ package simulatorui.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-public class ClientApplication
+public class ClientApplication extends SpringBootServletInitializer
 {
-    public static void main(String... args)
+    public static void main(String[] args) throws Exception
     {
         SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(ClientApplication.class);
     }
 }
--- src/main/resources/static/js/core/constants.js
+++ src/main/resources/static/js/core/constants.js
@@ -1,1 +1,2 @@
 App.constant('serverUrl', 'http://localhost:8092');
+//App.constant('serverUrl', 'http://localhost:9292/simulador'); change if war mode is used.
